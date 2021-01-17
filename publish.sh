d8f8024a837983b13461ad6e07370d64bf659f02
Friedger Müffke <mail@friedger.de>
2021-01-17 12:02:51 +0100

    Published at: So 17 Jan 2021 12:02:51 CET

---
+++ publish.sh
@@ -0,0 +1,7 @@
+cd ../../psq/stacks-dump
+node report /tmp/stacks-testnet-895e46fd342bf290/ -a > ../../friedger/pub-stacks-dump/stacks-dump.txt
+cd ../../friedger/pub-stacks-dump
+timestamp=$(date +%c)
+git add .
+git commit -m "Published at: $timestamp"
+git push origin main
