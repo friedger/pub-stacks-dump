95d744eb1fbfed9639eabdf7220fa45c9ce30b38
Friedger Müffke <mail@friedger.de>
2021-01-17 12:18:55 +0100

    Published at: So 17 Jan 2021 12:18:55 CET

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
+/home/friedger/_repos/github/friedger/pub-stacks-dump
 cd ../../psq/stacks-dump
 node report /tmp/stacks-testnet-895e46fd342bf290/ -a > ../../friedger/pub-stacks-dump/stacks-dump.txt
 cd ../../friedger/pub-stacks-dump
