66b66b0c7bced3553cb8e91a22f60df55612d280
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-01-18 08:55:21 -0700

    add og data and twitter cards

--- publish.sh
+++ publish.sh
@@ -11,6 +11,25 @@ cat > header.html <<EOF
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="theme-color" content="#5546FF" />
 <meta name="description" content="A public view of statistics around the Stacks blockchain, published hourly using stacks-dump." />
+<!-- OG:DATA -->
+<meta property="og:title" content="Stacks Dump" />
+<meta property="og:description" content="A public view of statistics around the Stacks blockchain, published hourly using stacks-dump." />
+<meta property="og:image" content="https://friedger.github.io/pub-stacks-dump/stacks-dump-truck.png" />
+<meta property="og:image:secure_url" content="https://friedger.github.io/pub-stacks-dump/stacks-dump-truck.png" />
+<meta property="og:image:type" content="image/png" />
+<meta property="og:image:width" content="512" />
+<meta property="og:image:height" content="340" />
+<meta property="og:image:alt" content="The stacks dump truck." />
+<meta property="og:site_name" content="Stacks Dump" />
+<meta property="og:type" content="website" />
+<meta property="og:locale" content="ALL_ALL" />
+<!-- TWITTER CARD -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@fmdroid">
+<meta name="twitter:description" content="A public view of statistics around the Stacks blockchain, published hourly using stacks-dump.">
+<meta name="twitter:title" content="Stacks Dump">
+<meta name="twitter:image" content="https://friedger.github.io/pub-stacks-dump/stacks-dump-truck.png">
+<meta name="twitter:image:alt" content="The stacks dump truck.">
 <style>
 body {background-color: #5546FF; color:#EEE; font-family: sans-serif;}
 pre {background-color: #000; padding: 4px; overflow:auto;}
